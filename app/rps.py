9aee8282d538d4d2f51670bf3c9fb29f33929c41
CanadiAnn <annie.lozeau@hotmail.com>
2020-12-20 22:53:42 +0000

    dropdown for player rendering

--- app/rps.py
+++ app/rps.py
@@ -3,23 +3,23 @@ import random
 rps = ["rock", "paper", "scissors"]
 
 player1 = "scissors"
-player2 = random.choice(rps)
+player2 = random.choice(rps)  #machine
 
-print("Player 1:", player1)
-print("Player 2:", player2)
+# print("Player 1:", player1)
+# print("Player 2:", player2)
 
 
 if player1 == player2:
     print("Null")
 elif player1 == "paper" and player2 == "rock":
-    print("Paper surrounds the rock: Player 1 wins!")
+    print("Paper covers rock: You win!")
 elif player1 == "rock" and player2 == "paper":
-    print("Paper surrounds the rock: Player 2 wins!")
+    print("Paper covers rock: You lose!")
 elif player1 == "scissors" and player2 == "rock":
-    print("Rock breaks the scissors: Player 2 wins!")
+    print("Rock crushes scissors: You lose!")
 elif player1 == "rock" and player2 == "scissors":
-    print("Rock breaks the scissors: Player 1 wins!")    
+    print("Rock crushes scissors: You win!")    
 elif player1 == "paper" and player2 == "scissors":
-    print("Scissors cut the paper: Player 2 wins!")
+    print("Scissors cuts paper: You lose!")
 elif player1 == "scissors" and player2 == "paper":
-    print("Scissors cut the paper: Player 1 wins!")
+    print("Scissors cuts paper: You win!")
