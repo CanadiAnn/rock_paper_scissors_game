8dbb946f239d54517af276f98016bda9e543c6bf
CanadiAnn <annie.lozeau@hotmail.com>
2020-12-28 12:34:49 +0000

    models completed

---
+++ app/models/rps.py
@@ -0,0 +1,22 @@
+# import random
+
+# rps = ["rock", "paper", "scissors"]
+# computer = random.choice(rps)  #machine
+
+class Rps:
+
+    def play(self, player, computer): 
+        if player == computer:
+            print("Null")
+        elif player == "paper" and computer == "rock":
+            print("Paper covers rock: You win!")
+        elif player == "rock" and computer == "paper":
+            print("Paper covers rock: You lose!")
+        elif player == "scissors" and computer == "rock":
+            print("Rock crushes scissors: You lose!")
+        elif player == "rock" and computer == "scissors":
+            print("Rock crushes scissors: You win!")    
+        elif player == "paper" and computer == "scissors":
+            print("Scissors cuts paper: You lose!")
+        elif player == "scissors" and computer == "paper":
+            print("Scissors cuts paper: You win!")
